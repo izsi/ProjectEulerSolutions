bab03c1303fee8a66a09930b241cb8b7619d312c
izsi <isabel.kuruma@gmail.com>
2014-10-11 02:24:41 +0100

    Improved the solution to Problem1
    Extracted out calculation of the sum of multiples of n into a separate
    function.

--- Problem1.java
+++ Problem1.java
@@ -5,25 +5,23 @@ public class Problem1 {
   
   public static void main(String[] args) { 
     
-    int sumMultiplesOfThree = 0;
-    for (int a = 1; a <= 333; a++) {
-      sumMultiplesOfThree = sumMultiplesOfThree + (a * 3);
-    }
+    int sumMultiplesOfThree = addMultiples(3);
     
-    int totalSum = sumMultiplesOfThree;
-    for (int b = 1; b <= 199; b ++) { 
-      totalSum = totalSum + (b * 5);
-    }
-     
-    int sumMultiplesOfFifteen = 0;
-    for (int c = 1; c <= 66; c ++) {
-      sumMultiplesOfFifteen = sumMultiplesOfFifteen + (c * 15);
-    }
+    int sumMultiplesOfFive = addMultiples(5);
     
-    System.out.println(totalSum - sumMultiplesOfFifteen);
+    int sumMultiplesOfFifteen = addMultiples(15);
+    
+    System.out.println(sumMultiplesOfThree + sumMultiplesOfFive - sumMultiplesOfFifteen);
     
   }
   
+  private static int addMultiples(int number) {
+    int sumMultiples = 0;
+    for (int a = 1; a <= (999/number); a ++) {
+      sumMultiples = sumMultiples + (a * number);
+    }
+    return sumMultiples;
+  }
 }
 
   
